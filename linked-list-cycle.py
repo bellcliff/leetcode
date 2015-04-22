9bba2e9468775fd3c2aad88fd64a12d9f154bb00
Yang Bo <yangb@garena.com>
2015-04-22 16:54:44 +0800

    linked list cycle

---
+++ linked-list-cycle.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        n1 = head
+        n2 = head
+        while n2 is not None and n2.next is not None:
+            n1 = n1.next
+            n2 = n2.next.next
+            if n1 == n2:
+                return True
+        return False
